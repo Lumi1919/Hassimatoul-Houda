9dc6e641942bcefa5c8919894e8e4eb99bce897c
Lumi1919 <82605764+Lumi1919@users.noreply.github.com>
2021-05-16 16:25:28 +0200

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -131,7 +131,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR, 'static',)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
